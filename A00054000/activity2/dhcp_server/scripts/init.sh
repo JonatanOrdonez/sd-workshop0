2f1052999af27dbb3580cb96198e1e0fd72b51a2
Jonatan Ordonez <jonatan-ordonez@hotmail.com>
2018-08-15 19:56:05 -0500

    Update init

--- A00054000/activity2/dhcp_server/scripts/init.sh
+++ A00054000/activity2/dhcp_server/scripts/init.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-yum update
 yum install dhcp -y
 yum install wget -y
 wget https://raw.githubusercontent.com/JonatanOrdonez/sd-workshop0/jordonez/workshop0/A00054000/activity2/dhcp_server/scripts/dhcpd.conf -O /etc/dhcp/dhcpd.conf
